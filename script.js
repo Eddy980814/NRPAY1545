fd7ceba97db0ebc09f8c06387a5329e038329aca
Eddy980814 <Wag123890@gmail.com>
2025-03-04 23:44:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const users = {};
-for (let i = 1; i <= 20; i++) {
-  users[`user${i}`] = { password: `pass${i}`, balance: 1000 };
-}
+const users = {
+  'ECX': { password: 'A4uu7014', balance: 1000 },
+  'user2': { password: 'def456', balance: 2000 },
+  'user3': { password: 'ghi789', balance: 3000 },
+  'user4': { password: 'jkl012', balance: 4000 },
+  'user5': { password: 'mno345', balance: 5000 },
+  'user6': { password: 'pqr678', balance: 6000 },
+  'user7': { password: 'stu901', balance: 7000 },
+  'user8': { password: 'vwx234', balance: 8000 },
+  'user9': { password: 'yz5678', balance: 9000 },
+  'user10': { password: '123abc', balance: 10000 },
+  'user11': { password: '456def', balance: 11000 },
+  'user12': { password: '789ghi', balance: 12000 },
+  'user13': { password: '012jkl', balance: 13000 },
+  'user14': { password: '345mno', balance: 14000 },
+  'user15': { password: '678pqr', balance: 15000 },
+  'user16': { password: '901stu', balance: 16000 },
+  'user17': { password: '234vwx', balance: 17000 },
+  'user18': { password: '567yz', balance: 18000 },
+  'user19': { password: 'abc890', balance: 19000 },
+  'user20': { password: 'def123', balance: 20000 },
+};
 
 let currentUser = null;
 
